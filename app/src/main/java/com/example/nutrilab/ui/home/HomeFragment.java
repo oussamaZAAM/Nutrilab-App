6cdab96904810f0a9857499278c84f55b374739c
oussamaZAAM <zaam.oussama@gmail.com>
2023-05-13 21:50:55 +0100

    Gender interface + Back next buttons

--- app/src/main/java/com/example/nutrilab/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/nutrilab/ui/home/HomeFragment.java
@@ -3,10 +3,13 @@ package com.example.nutrilab.ui.home;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
@@ -17,9 +20,12 @@ import com.example.nutrilab.R;
 public class HomeFragment extends Fragment {
 
     private EditText ageEditText;
-    private TextView ageTitle;
+    private TextView infosTitle;
 
-    private RadioGroup genderRadioGroup;
+    private LinearLayout genderChoice;
+    private ImageView maleIcon;
+    private ImageView femaleIcon;
+
     private EditText heightEditText;
     private EditText weightEditText;
     private Button nextButton;
@@ -30,35 +36,73 @@ public class HomeFragment extends Fragment {
     private float height;
     private float weight;
 
-    private boolean isAgeInterface = true;
-
-    @SuppressLint("CutPasteId")
+    @SuppressLint({"CutPasteId", "ClickableViewAccessibility"})
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         ageEditText = view.findViewById(R.id.ageEditText);
-        ageTitle = view.findViewById(R.id.ageTitle);
+        infosTitle = view.findViewById(R.id.infosTitle);
 
-        genderRadioGroup = view.findViewById(R.id.genderRadioGroup);
+        genderChoice = view.findViewById(R.id.genderChoice);
+        maleIcon = view.findViewById(R.id.maleIcon);
+        femaleIcon = view.findViewById(R.id.femaleIcon);
+
         heightEditText = view.findViewById(R.id.heightEditText);
         weightEditText = view.findViewById(R.id.weightEditText);
+
         nextButton = view.findViewById(R.id.nextButton);
         backButton = view.findViewById(R.id.backButton);
 
-        EditText myEditText = view.findViewById(R.id.ageEditText);
-        myEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//        maleIcon.setOnHoverListener(new View.OnHoverListener() {
+//            @Override
+//            public boolean onHover(View v, MotionEvent event) {
+//                switch (event.getAction()) {
+//                    case MotionEvent.ACTION_DOWN:
+//                        // Do something when the image is touched
+//                        maleIcon.setBackgroundResource(R.drawable.male_background);
+//                        break;
+//                    case MotionEvent.ACTION_UP:
+//                        // Do something when the touch is released
+//                        maleIcon.setBackgroundResource(0);
+//                        break;
+//                }
+//                return true;
+//            }
+//        });
+
+        maleIcon.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    myEditText.setBackgroundResource(R.drawable.edittext_border);
-                } else {
-                    myEditText.setBackgroundResource(R.drawable.edittext_focused);
+            public void onClick(View v) {
+                gender = "male";
+                if (gender == "male"){
+                    maleIcon.setBackgroundResource(R.drawable.male_background);
+                    femaleIcon.setBackgroundResource(0);
                 }
             }
         });
 
+        femaleIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gender = "female";
+                if (gender == "female"){
+                    maleIcon.setBackgroundResource(0);
+                    femaleIcon.setBackgroundResource(R.drawable.female_background);
+                }
+            }
+        });
 
+        ageEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    ageEditText.setBackgroundResource(R.drawable.edittext_border);
+                } else {
+                    ageEditText.setBackgroundResource(R.drawable.edittext_focused);
+                }
+            }
+        });
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,12 +111,24 @@ public class HomeFragment extends Fragment {
                     // Get the value of the age field and store it in a variable
                     age = Integer.parseInt(ageEditText.getText().toString());
 
-                    ageTitle.setText("What's your gender");
+                    if (age >= 12){
+                        infosTitle.setText("What's your gender?");
 
-                    // Hide the age field and show the gender field
-                    ageEditText.setVisibility(View.GONE);
-                    genderRadioGroup.setVisibility(View.VISIBLE);
-                    backButton.setVisibility(View.VISIBLE);
+                        // Hide the age field and show the gender field
+                        ageEditText.setVisibility(View.GONE);
+                        genderChoice.setVisibility(View.VISIBLE);
+                        backButton.setVisibility(View.VISIBLE);
+                    }
+                }
+
+                if (genderChoice.getVisibility() == View.VISIBLE) {
+                    if (gender == "male" || gender == "female"){
+                        infosTitle.setText("What's your Height and Weight?");
+
+                        genderChoice.setVisibility(View.GONE);
+                        heightEditText.setVisibility(View.VISIBLE);
+                        weightEditText.setVisibility(View.VISIBLE);
+                    }
                 }
             }
         });
@@ -80,16 +136,26 @@ public class HomeFragment extends Fragment {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (genderRadioGroup.getVisibility() == View.VISIBLE) {
+                if (genderChoice.getVisibility() == View.VISIBLE) {
                     // Show the age field and hide the gender field
                     ageEditText.setVisibility(View.VISIBLE);
-                    genderRadioGroup.setVisibility(View.GONE);
-                    backButton.setVisibility(View.GONE);
-                    ageTitle.setText("How old are you?");
+                    genderChoice.setVisibility(View.GONE);
+                    backButton.setVisibility(View.INVISIBLE);
+                    infosTitle.setText("How old are you?");
                 }
-//                heightEditText.setVisibility(View.VISIBLE);
+
+                if (heightEditText.getVisibility() == View.VISIBLE) {
+                    // Show the age field and hide the gender field
+                    heightEditText.setVisibility(View.GONE);
+                    weightEditText.setVisibility(View.GONE);
+
+                    genderChoice.setVisibility(View.VISIBLE);
+                    infosTitle.setText("What's your gender?");
+                }
             }
         });
+
+        System.out.println("Age: "+age+", Gender: "+gender);
 
         return view;
     }
