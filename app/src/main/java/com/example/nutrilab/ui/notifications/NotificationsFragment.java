2e98e8bceeee05446ec094a589f4c747fc6f68c4
oussamaZAAM <zaam.oussama@gmail.com>
2023-05-22 23:19:52 +0100

    Fix Diet page Styling (Overlapping)

--- app/src/main/java/com/example/nutrilab/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/nutrilab/ui/notifications/NotificationsFragment.java
@@ -4,20 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.nutrilab.R;
 import com.example.nutrilab.databinding.FragmentNotificationsBinding;
-import com.example.nutrilab.ui.food.FoodListAdapter;
 import com.example.nutrilab.ui.general.SharedPrefsHelper;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class NotificationsFragment extends Fragment {
     private static final String PREFS_NAME = "MyPrefs";
@@ -26,11 +24,18 @@ public class NotificationsFragment extends Fragment {
     private FragmentNotificationsBinding binding;
     private TextView textNotifications;
     private ListView newDietListView;
+    private ImageButton backButton;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notifications, container, false);
 
         NewDietAdapter newDietAdapter = new NewDietAdapter(requireContext(), SharedPrefsHelper.loadMap(requireContext(), PREFS_NAME, FOOD_DIET), SharedPrefsHelper.loadSet(requireContext(), PREFS_NAME, FOOD_EATEN));
+
+        backButton = view.findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(view);
+            navController.navigate(R.id.navigation_food);
+        });
 
         newDietListView = view.findViewById(R.id.new_diet);
         newDietListView.setAdapter(newDietAdapter);
